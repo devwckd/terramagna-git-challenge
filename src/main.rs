6bbaa42c3f6e9658c14cbdcaa0042e66f05f0241
BuildTools <unconfigured@null.spigotmc.org>
2022-04-14 13:24:23 -0300

    feat(axum): create foundation
    create axum's router and server foundation.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::SocketAddr;
+
+use axum::Router;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new();
+
+    axum::Server::bind(&SocketAddr::from(([127, 0, 0, 1], 8080)))
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
